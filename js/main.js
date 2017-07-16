a9ba9fb4e0b8b2fd654d085962cbae348497e5a6
PageAberdeen <zhaoyige@zhaoyigedeMacBook-Pro.local>
2017-07-16 16:31:55 +0800

    增加验证规则

--- js/main.js
+++ js/main.js
@@ -7,6 +7,11 @@ $().ready(function(){
     var flag = "<option value='"+i+"'>" +department[i]+"</option>"
     $('#department').append(flag)
   }
+  var company = ['柳州市总工会','柳州质检','柳州人设','..'] //数据库取值生成数组
+  for(var i = 0 ; i < company.length; i++) {
+    var flag = "<option value='"+i+"'>" +company[i]+"</option>"
+    $('#company').append(flag)
+  }
   function validform(obj){
     return $(obj).validate({
       rules:{
@@ -31,6 +36,54 @@ $().ready(function(){
           required: true,
           chinese: true
         },
+        //巡检运维表单验证
+        unitName: {
+          required: true,
+          selectNone: true
+        },
+        category: {
+          required: true,
+          selectNone: true
+        },
+        InspectionDate: {
+          required: true
+        },
+        InspectionContent: {
+          required: true
+        },
+        problemLog: {
+          required: true
+        },
+        treatmentMethods:{
+          required: true
+        },
+        //维修运维表单验证
+        address: {
+          required: true
+        },
+        repairDate: {
+          required: true
+        },
+        handleDate: {
+          required: true
+        },
+        repairContent: {
+          required: true
+        },
+        failureReport: {
+          required: true
+        },
+        resolvent: {
+          required: true
+        },
+        //新媒体运维表单验证
+        release: {
+          required: true
+        },
+        title: {
+          required: true
+        },
+        //教育运维表单验证
         icloudCategory: {
           required: true,
           selectNone: true
@@ -64,7 +117,34 @@ $().ready(function(){
     }
   })
 
-  /*注册页面验证*/
+  /*巡检页面验证*/
+  $('.js-Inspection').on('click',function(){
+    if(validform('#Inspection').form()){ //通过验证之后执行
+      alert('提交成功')
+      window.location.href="admin.html"
+    }else{
+
+    }
+  })
+  /*维修页面验证*/
+  $('.js-service').on('click',function(){
+    if(validform('#service').form()){ //通过验证之后执行
+      alert('提交成功')
+      window.location.href="admin.html"
+    }else{
+
+    }
+  })
+  /*新媒体验证*/
+  $('.js-newMedia').on('click',function(){
+    if(validform('#newMedia').form()){ //通过验证之后执行
+      alert('提交成功')
+      window.location.href="admin.html"
+    }else{
+
+    }
+  })
+  /*教育页面验证*/
   $('.js-icloud').on('click',function(){
     if(validform('#icloud').form()){ //通过验证之后执行
       alert('提交成功')
